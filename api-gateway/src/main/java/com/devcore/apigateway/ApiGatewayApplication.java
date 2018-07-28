9356fbee51f544d3f553e7ed2e4e583a406a5968
YarielInfante <yarielinfante@gmail.com>
2018-07-28 14:10:13 -0400

    config server feature

--- api-gateway/src/main/java/com/devcore/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/devcore/apigateway/ApiGatewayApplication.java
@@ -1,11 +1,15 @@
 package com.devcore.apigateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,6 +17,19 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @EnableHystrixDashboard
 public class ApiGatewayApplication {
+
+    @RestController
+    class RestConfig {
+
+        @Value("${app.greet}")
+        String greeting;
+
+        @RefreshScope
+        @GetMapping("/greet")
+        String greet(){
+            return greeting;
+        }
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
---
+++ configuration-server/src/main/java/com/devcore/library/config/ConfigServerApp.java
@@ -0,0 +1,15 @@
+package com.devcore.library.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApp.class, args);
+    }
+}
